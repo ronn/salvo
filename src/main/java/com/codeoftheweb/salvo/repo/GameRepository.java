a8c744cdd3f052406c33472660cac32d7ed67377
ronniecanas <ronniecanas@seven4n.com>
2018-10-16 11:21:39 +0200

    first commit

--- src/main/java/com/codeoftheweb/salvo/repo/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeoftheweb.salvo.repo;
-
-import com.codeoftheweb.salvo.entity.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
