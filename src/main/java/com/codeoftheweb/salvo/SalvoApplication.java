8c9064dba23ff29fdf9b92faef2f74feb21c59f4
ronniecanas <ronniecanas@seven4n.com>
2018-10-16 17:51:41 +0200

    Task 1 Done

--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -1,12 +1,26 @@
 package com.codeoftheweb.salvo;
 
+import com.codeoftheweb.salvo.entity.Player;
+import com.codeoftheweb.salvo.repo.PlayerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SalvoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository playerRepo){
+		return args -> {
+			playerRepo.save(new Player("j.bauer@ctu.com"));
+			playerRepo.save(new Player("c.obria@ctu.com"));
+			playerRepo.save(new Player("k.imbau@ctu.com"));
+			playerRepo.save(new Player("t.almei@ctu.com"));
+		};
 	}
 }
---
+++ src/main/java/com/codeoftheweb/salvo/entity/Player.java
@@ -0,0 +1,41 @@
+package com.codeoftheweb.salvo.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String userName;
+
+    public Player() {
+    }
+
+    public Player(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
